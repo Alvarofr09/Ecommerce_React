67500da9911668012efe9c4c80c048eb87442b02
Alvarofr09 <alvarofriasruiz03@gmail.com>
2024-03-22 19:15:41 +0100

    Importar componentes generales

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { Outlet } from "react-router-dom";
+
+import Header from "./components/Genereales/Header";
 import Navigation from "./components/Genereales/Navigation";
+import Footer from "./components/Genereales/Footer";
 
 import "./App.css";
 
 function App() {
 	return (
 		<>
+			<Header />
 			<Navigation />
+			<Outlet />
+			<Footer />
 		</>
 	);
 }
