ee349b0bca4df5c3486c3aea7b1ce068616e5eed
Alvarofr09 <alvarofriasruiz03@gmail.com>
2024-03-22 18:03:50 +0100

    Not found

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import App from "./App.jsx";
+import NotFound from "./views/Generales/NotFound.jsx";
+
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<Routes>
-				<Route path="/*" element={<App />} />
+				<Route path="/" element={<App />} />
+				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</BrowserRouter>
 	</React.StrictMode>
