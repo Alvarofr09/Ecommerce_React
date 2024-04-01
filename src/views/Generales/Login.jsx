ed6f72f7ed3c6666330066d6827d84901ad4dba7
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-01 10:02:18 +0200

    Importar componente

--- src/views/Generales/Login.jsx
+++ src/views/Generales/Login.jsx
@@ -1,7 +1,10 @@
+import Form from "../../components/Form/Form";
+
 export default function Login() {
 	return (
 		<div>
-			<h1>Aqui va el login y registro</h1>
+			<Form />
+			<Form />
 		</div>
 	);
 }
