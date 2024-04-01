5503acf25fff404b707f814dda6887fc63ede4a5
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-01 10:01:51 +0200

    Layout general

---
+++ src/components/Layout.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Header from "./Header/Header";
+import Navigation from "./Navigation/Navigation";
+import Adjetivos from "./Adjetivos/Adjetivos";
+import Footer from "./Footer/Footer";
+
+export default function Layout() {
+	return (
+		<>
+			<Header />
+			<Navigation />
+			<Outlet />
+			<Adjetivos />
+			<Footer />
+		</>
+	);
+}
