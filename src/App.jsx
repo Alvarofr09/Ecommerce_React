00ecb3b69dadf3f052749bb294c2d23fc7a5473f
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-01 09:19:56 +0200

    Cambiar rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { Outlet } from "react-router-dom";
 
-import Header from "./components/Genereales/Header";
-import Navigation from "./components/Genereales/Navigation";
-import Adjetivos from "./components/Genereales/Adjetivos";
-import Footer from "./components/Genereales/Footer";
+import Header from "./components/Generales/Header";
+import Navigation from "./components/Generales/Navigation";
+import Adjetivos from "./components/Generales/Adjetivos";
+import Footer from "./components/Generales/Footer";
 
 import "./App.css";
 
